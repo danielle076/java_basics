20a4bd103c085347ea51695ff9065c016d1e1391
danielle076 <d.vandenakker@novi-education.nl>
2021-01-02 17:16:14 +0100

    added if then

--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 
-In the src folder you will find the following:
+**In the src folder you will find the following:**
 1. Hello World
 2. Byte, short, int and long + challenge
 3. Float and double + challenge
 4. Char and boolean
 5. String
 6. Operators
+7. If Then
---
+++ src/IfThen/Main.java
@@ -0,0 +1,25 @@
+package IfThen;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // = is an assignment operator
+        // == asks if the operands (isAlien) are identical or equal to each other
+        boolean isAlien = false;
+        if (isAlien == false) // important: if statement doesn't have ';'. It doesn't give an error message if you do
+            System.out.println("It is not an alien"); // It is not an alien
+
+        boolean isMonster = false;
+        if (isMonster == true)
+            System.out.println("It is not a monster"); // doesn't print this out
+            System.out.println("I'm scared of monsters"); // print this, even though == is true (it shouldn't print anything)
+            // with a codeblock {} he looks at both lines, now only at the first one and prints the second
+
+        // if you use codeblock {} he won't print out both lines
+        boolean isGhost = false;
+        if (isGhost == true) {
+            System.out.println("It is not a ghost");
+            System.out.println("I'm scared of ghosts");
+        }
+    }
+}
